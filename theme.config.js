e3b38383f79ae90891c81c0d84fa5e8ce921b002
MDCDEV <me@mdcdev.me>
2022-10-01 17:31:06 -0500

    First version of Guide

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/MDCYT/Guilded-Bot',
+  docsRepositoryBase: 'https://github.com/MDCYT/Guilded-Bot-Guide',
+  titleSuffix: ' – Guilded-Bot',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline text-yello-500">Guilded-Bot</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Simple package for your Guilded-Bot
       </span>
     </>
   ),
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Guilded-Bot: Simple package for your Guilded-Bot" />
+      <meta name="og:description" content="Guilded-Bot: Simple package for your Guilded-Bot" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="Guilded-Bot: Simple package for your Guilded-Bot" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Guilded-Bot" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -61,7 +61,8 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerEditLink: 'Edit this page',
+  footerText: <p><p style={{color:"#E9BA00"}}>Guilded-Bot</p> © {new Date().getFullYear()} Guilded-Bot</p>,
   unstable_faviconGlyph: '👋',
+  darkMode: true,
 }
